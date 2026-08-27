d727171c532e8a9da476e22acab368aff768b2e3
sapammeded <sapammeded@gmail.com>
2026-08-27 20:58:32 +0700

    Add Android web asset preparation

---
+++ scripts/prepare-android.js
@@ -0,0 +1,1 @@
+const fs=require('fs'); const path=require('path'); fs.rmSync('www',{recursive:true,force:true}); fs.mkdirSync('www',{recursive:true}); fs.copyFileSync('stt.html',path.join('www','index.html'));
